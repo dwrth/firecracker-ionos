a4a55da466e0fa848f49685a4f7f8d8cd334cbfc
dwrth <github@dwrth.dev>
2026-08-30 14:08:13 +0200

    Add vm state model types

--- internal/state/doc.go
+++
@@ -1,2 +0,0 @@
-// Package state will manage the state of the system.
-package state
---
+++ internal/state/vm.go
@@ -0,0 +1,29 @@
+// Package state will manage the state of the system.
+package state
+
+type Status string
+
+const (
+	StatusStopped Status = "stopped"
+	StatusRunning Status = "running"
+)
+
+type VM struct {
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	UID           int    `json:"uid"`
+	GID           int    `json:"gid"`
+	VCPUs         int    `json:"vcpus"`
+	MemoryMiB     int    `json:"memory_mib"`
+	GuestIP       string `json:"guest_ip"`
+	GatewayIP     string `json:"gateway_ip"`
+	GuestSubnet   string `json:"guest_subnet"`
+	TransitHostIP string `json:"transit_host_ip"`
+	TransitNSIP   string `json:"transit_ns_ip"`
+	TransitSubnet string `json:"transit_subnet"`
+	Namespace     string `json:"namespace"`
+	HostVeth      string `json:"host_veth"`
+	NSVeth        string `json:"ns_veth"`
+	TAP           string `json:"tap"`
+	Status        Status `json:"status"`
+}
